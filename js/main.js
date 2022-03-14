4402764a09725957887154f1960c3c6546f04428
Ehsan040 <taheri.ehsan@web.de>
2022-03-14 11:27:14 +0100

    tools documentation

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-import { capitalizeFirstCharacter, getTodaysDate } from './tools.js';
+import * as tools from './tools.js';
 
 const contentElem = document.querySelector('.content');
 
 contentElem.innerHTML = ` 
-<p>${capitalizeFirstCharacter('this is a test')}</p>
+<p>${tools.capitalizeFirstCharacter('this is another test')}</p>
     <p>${getTodaysDate()}</p>
 
 
